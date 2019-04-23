ecd264b355cdbc115e0f93c5665b6ef7872b3ef9
Dora <isadorasd@al.insper.edu.br>
2019-04-23 10:41:09 -0300

    avanco

--- base.py
+++ base.py
@@ -45,6 +45,18 @@ class Player(pygame.sprite.Sprite):
         # Centraliza embaixo da tela.
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
+        
+        # Velocidade da nave
+        self.speedx = 0
+    
+    # Metodo que atualiza a posição da navinha
+    def update(self):
+        self.rect.x += self.speedx
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
 
 # Inicialização do Pygame.
 pygame.init()
@@ -86,6 +98,26 @@ try:
             # Verifica se foi fechado
             if event.type == pygame.QUIT:
                 running = False
+                
+            # Verifica se apertou alguma tecla.
+            if event.key == pygame.KEYDOWN:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    player.speedx = -8
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 8
+            
+            # Verifica se soltou alguma tecla
+            if event.key == pygame.KEYUP:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 0
+                    
+            # Depois de processar os eventos.
+            # Atualiza a ação de cada sprite.
+            all_sprite.update()
     
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
